1d6d6c8cdda7ed1883a7cfa33d2be28075b4a4f2
Eduardo Justi <edwjusti@outlook.com>
2023-05-07 20:08:47 -0300

    chore: deleted upstream files

--- src/api/fetchResource.ts
+++
@@ -1,8 +0,0 @@
-async function fetchResource(req: RequestInfo, opts?: RequestInit) {
-  const response = await fetch(req, opts);
-  const resource = await response.json();
-
-  return resource;
-}
-
-export default fetchResource;
--- src/util.ts
+++
@@ -1,9 +0,0 @@
-export const dateFormatter = new Intl.DateTimeFormat('en-US', {
-  year: 'numeric',
-  month: 'long',
-  day: 'numeric',
-});
-export const numberFormatter = new Intl.NumberFormat('en-US');
-
-export const capitalize = (str: string) =>
-  str.charAt(0).toUpperCase() + str.slice(1);
